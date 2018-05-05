ddefc21a997792cdda244fadfce299419026a55e
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-05-04 19:33:33 -0700

    Some changes

--- ui/config/.env.sample.js
+++ ui/config/.env.sample.js
@@ -6,11 +6,10 @@ const envBuild = {
     'NODE_ENV=DEVELOPMENT',
     // 'DEBUG=TRUE',
     'ENVPREFIX=REACT_APP_',
-    'REST_SERVER_URL=http://localhost:4990',
-    'SOCKET_SERVER_URL=http://localhost:4155',
-    'CODERUNNER_SERVICE_URL=http://localhost:4000',
-    'REACT_APP_SOCKET_SERVER_URL=http://localhost:4155',
-    'REACT_APP_REST_SERVER_URL=http://localhost:4990'
+    'REST_SERVER_URL=http://localhost:3000',
+    'SOCKET_SERVER_URL=http://localhost:4000',
+    'REACT_APP_SOCKET_SERVER_URL=http://localhost:4000',
+    'REACT_APP_REST_SERVER_URL=http://localhost:3000'
   ]
 }
 
