06aa433931f41b6d626cdf3c873d3379bf490619
Hunter Morgenstern <hunter.morgenstern@gmail.com>
2018-05-11 19:33:01 -0700

    woahhh storing cart in redis, have backend for grabbing cart

--- api/rest-server/src/components/cart/cartControllers.js
+++ api/rest-server/src/components/cart/cartControllers.js
@@ -10,9 +10,9 @@ export const cartAddController = {
   addItem: async (req, res) => {
   try {
    
-    const userId = req.body.userId
-    const item = req.body.item
-    const quantity = req.body.quantity
+    const userId = req.body.userId;
+    const item = req.body.item;
+    const quantity = req.body.quantity;
     const addedItem = await client.hset(userId, item, quantity);
     success('cartController - successfully added item to redis cart');
     return res.status(200).send('item added');
@@ -37,5 +37,36 @@ getCart: async (req, res) => {
     error('cartController - grab cart error= ', err);
     throw new Error(err);
   }
-}
+},
+
+sendOrder: async (req, res) => {
+  try {   
+    const bizId = req.body.bizId
+    const userId = req.body.userId
+    const order = req.body.order
+    const addedItem = await client.hset(bizId, userId, order);
+    success('cartController - successfully added order to the redis');
+    return res.status(200).send('woo order happened');
+  
+  } catch (err) {
+    error('cartController - order error= ', err);
+    throw new Error(err);
+  }
+},
+
+grabBizOrders: async (req, res) => {
+  try {
+     await client.hgetall(req.params.bizId, (err, orders) => {
+      if(err) {
+        error('error retrieving orders', err);
+      }
+      return res.status(200).send(orders);
+    });
+    success('cartController - successfully grabbed orders');
+  
+  } catch (err) {
+    error('cartController - grab orders error= ', err);
+    throw new Error(err);
+  }
+},
 }
