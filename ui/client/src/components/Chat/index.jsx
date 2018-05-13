fb8523d701d933e4cdc06b2bb764e0eb40f1a3bb
DK <dk@Justin-Kangs-MacBook-Air.local>
2018-05-12 17:58:26 -0700

    [edit] working on sockets

---
+++ ui/client/src/components/Chat/index.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Messages from '../messages/index.jsx';
+
+import axios from 'axios';
+import io from 'socket.io-client';
+const socket = io('http://localhost:4000')
+
+class Chat extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      messages : [],
+      text : ''
+    }
+  }
+
+  onTextChange(e) {
+    console.log('this is the state', this.state.text)
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
+  handleKeyPress = async (e) => {
+    if(e.key === 'Enter') {
+      console.log('enter button has been clicked')
+    }
+  }
+
+  render() {
+    return(
+      <div>
+        WELCOME TO THE CHAT PAGE
+        <div>
+        <Messages/>
+        </div>
+
+        <form>
+          <input type='text' name='text' onChange={this.onTextChange.bind(this)} onKeyPress={this.handleKeyPress.bind(this)} placeholder='send message..'/>
+        </form>
+      </div>
+    )
+  }
+};
+
+export default Chat;
---
+++ ui/client/src/components/messages/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Messages extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
+  render() {
+    return(
+      <div>
+        this is the messages
+      </div>
+    )
+  }
+};
+
+export default Messages;
