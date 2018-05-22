bd0276a3551532ff9d7ffa1c11b749186a75e608
IsabellaBeltran <isabellabeltran79@gmail.com>
2018-05-21 22:28:47 -0700

    Charging user complete using real data

--- api/rest-server/cron/cronController.js
+++ api/rest-server/cron/cronController.js
@@ -1,6 +1,8 @@
 const cron = require('cron');  
 const stripe = require('stripe')(process.env.SECRET_KEY);
 const Order = require('../src/config/databases/mongo/orders/ordersModel');
+import bluebird from 'bluebird';
+bluebird.promisifyAll(client);
 const { Schema } = require('mongoose'); 
 import client from '../redis'; 
 
@@ -12,41 +14,63 @@ import {
   getUserPaymentIdAndEmail
 } from './cronQueries.js'; 
 
-/**
- * I need to grab poolID from redis 
- * for each poolID grab all the users ID and the total 
- * for each user make the charge 
- *  Get user's paymentid from the db and email 
- *  
- */
-
 
 const chargeUser = new cron.CronJob({
-  cronTime: '* * * * * ',
+  cronTime: '* * * * * *',
   onTick: async () => {
     try {
-//look for users ID and return email and paymentid on redis? 
-//Case: if for some reason user doesn't have paymentid ? 
-//GRAB USERS POOL 
-//GRAB USERS ID 
-//Grab users email and stripeaccount to make the charge 
-      const body = {
-        id: 3
-      }
-      const { stripeaccount, email } = await getUserPaymentIdAndEmail(body); 
-
-//Charge each customer 
-    const charge = await stripe.charges.create({
-    amount: 50, // This is in cents
-    currency: 'usd',
-    source: `${stripeaccount}`,
-    receipt_email: `${email}`,
-  });
-  console.log('Charge customer', charge); 
-    success('Successfully charge users in each pool order'); 
-  } catch(err) {
-    error('Error from cronController inside charge user', err); 
-  }
+      let count; 
+      let discount;  
+      let userId; 
+      let total; 
+      const poolIds = await client.smembersAsync('allPools')
+        const pools = [];
+        for (let i = 0; i < poolIds.length; i++) {
+          let poolData = await client.hgetallAsync(poolIds[i]);
+          count = poolData.count; 
+          let allOrder = await client.hgetallAsync(poolData.bizId); 
+          for (var key in allOrder) {
+            userId = key; //Will be in the format poolId:userId 
+            let subtotal = 0;
+            let order = JSON.parse(allOrder[key]);
+              for (var item in order) {
+                let quantity = JSON.parse(order[item])[1];
+                let price = JSON.parse(order[item])[0];
+                subtotal += price * quantity;
+              }
+              if(count < 5) {
+                discount = 1; 
+              }
+              if(count < 10 && count > 5) {
+                discount = 1; 
+              }
+              if(count < 15 && count > 10) {
+                discount = .95;
+              }
+              if(count < 20 && count > 15) {
+                discount = .9;
+              }
+              if(count > 20) {
+                discount = .85;
+              }
+              total = discount * subtotal * 100; 
+            }
+              const body = {
+                id: userId
+              };
+              const { stripeaccount, email } = await getUserPaymentIdAndEmail(body); 
+              const charge = await stripe.charges.create({
+              amount: Math.floor(total), // This is in cents
+              currency: 'usd',
+              source: `${stripeaccount}`,
+              receipt_email: `${email}`, 
+            });
+            console.log('Charge customer', charge); 
+              success('Successfully charge users in each pool order'); 
+        } 
+      } catch(err) {
+          error('Error from cronController inside charge user', err); 
+        }
   },
   start: false,
   timeZone: 'America/Los_Angeles'
@@ -64,6 +88,7 @@ const userOrderHistory = new cron.CronJob({
   onTick: async () => {
     //Save each user order on mongoDB 
     //Get information from redis 
+    
     try {
       const order = new Order({
         userId: 3,
